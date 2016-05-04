f012db8d8b0fe1a6272e2e6dbe28fe683c854261
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-05-04 12:51:17 +0500

    add singleton pattern to Logger

--- app.rb
+++ app.rb
@@ -1,13 +1,29 @@
 class Logger
+  def initialize
+    @f = File.open('log.txt', 'a')
+  end
+
+ #class variable
+  @@a = nil
+  @b = 2 # instance variable
+
+  def self.instance
+    if @@a == nil
+      @@a = Logger.new
+    end
+
+    return @@a
+  end
+
+  # class method
   def self.say_something
     puts 'hehe'
   end
 
-  def self.log_something(what)
-    f = File.open('log.txt', 'a')
-    f.puts what
-    f.close
+  # instance method
+  def log_something(what)
+    @f.puts what
   end
 end
 
-Logger.log_something('logged action')
+Logger.instance.log_something('logged action')
