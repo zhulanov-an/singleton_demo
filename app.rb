cf394d371a9d657f62f76897a8bf0b6deaf9494b
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-05-04 12:52:32 +0500

    remove recycle

--- app.rb
+++ app.rb
@@ -5,20 +5,14 @@ class Logger
 
  #class variable
   @@a = nil
-  @b = 2 # instance variable
 
   def self.instance
     if @@a == nil
       @@a = Logger.new
     end
-
     return @@a
   end
 
-  # class method
-  def self.say_something
-    puts 'hehe'
-  end
 
   # instance method
   def log_something(what)
