c8a1cdcf6e3aba6bfabf4d49a1320e4d928c4c63
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-05-04 11:36:32 +0500

    add log to file

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ class Logger
   def self.say_something
     puts 'hehe'
   end
+
+  def self.log_something(what)
+    f = File.open('log.txt', 'a')
+    f.puts what
+    f.close
+  end
 end
 
-Logger.say_something
+Logger.log_something('logged action')
